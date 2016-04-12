ea876b19f1b52c9e74acb0c657e47c53b9ff6b7d
Julian <julianadamwise@gmail.com>
2016-04-12 15:54:01 +1000

    Add content

---
+++ whatevs.py
@@ -0,0 +1,19 @@
+import sys,re
+
+def main():
+    fh = open(sys.argv[1],'r')
+    lines = fh.readlines()
+    print '\tpython2.2 -c "`printf \\"if 1:\\n\\'
+    for line in lines:
+        line = re.sub('[\\\'\"()]','\\\g<0>',line)
+        # grab leading white space (should be multiples of 4) and makes them into
+        # tabs
+        wh_spc_len = len(re.match('\s*',line).group())
+
+        sys.stdout.write('\t')
+        sys.stdout.write(wh_spc_len/4*'\\t'+line.rstrip().lstrip())
+        sys.stdout.write('\\n\\\n')
+    print '\t\\"`"'
+
+if __name__=='__main__':
+    main()
